20e9deda2a02e618270cd5372dec84a55f8e3305
Denk <aram.denk@siemens.com>
2022-11-07 14:31:39 +0100

    Created controllers

---
+++ src/Server/Controllers/MatchController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TournamentManager.Server.Data;
+using TournamentManager.Shared.Models;
+
+namespace TournamentManager.Server.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class MatchController : AuthorizedControllerBase
+{
+    public MatchController(TournamentManagerDbContext dbContext) : base(dbContext)
+    {
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<List<MatchModel>>> Get()
+    {
+          return Ok(await DbContext.Matches.ToListAsync());
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<MatchModel>> GetById(Guid id)
+    {
+        var match = await DbContext.Matches.FirstOrDefaultAsync(x => x.Id == id);
+        if (match is null)
+            return NotFound();
+
+        return Ok(match);
+    }
+
+    [HttpGet("{tournamentId:guid}")]
+    public async Task<ActionResult<MatchModel>> GetByTournamentId(Guid id)
+    {
+        var match = await DbContext.Matches.Where(x => x.TournamentId == id);
+        if (match is null)
+            return NotFound();
+
+        return Ok(match);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> InsertOrUpdate([FromBody] MatchModel? match)
+    {
+        if (match is null || match.TournamentId is null )
+            return BadRequest();
+
+        return Ok(await DbContext.Matches.AddAsync(match));
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var match = await DbContext.Matches.FirstOrDefaultAsync(x => x.Id == id);
+        if (match is null)
+            return NoContent();
+
+        if (match.Tournament.CreatorId != LoggedUser.Id && !LoggedUser.IsAdministrator)
+            return Forbid();
+
+        DbContext.Remove(match);
+        await DbContext.SaveChangesAsync();
+
+        return Ok();
+    }
+}
---
+++ src/Server/Controllers/TeamIsParticipatingController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TournamentManager.Server.Data;
+using TournamentManager.Shared.Models;
+
+namespace TournamentManager.Server.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class TeamIsParticipatingController : AuthorizedControllerBase
+{
+    public TeamIsParticipatingController(TournamentManagerDbContext dbContext) : base(dbContext)
+    {
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<List<TeamIsParticipatingModel>>> Get()
+    {
+          return Ok(await DbContext.Participatings.ToListAsync());
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<TeamIsParticipatingModel>> GetById(Guid id)
+    {
+        var participation = await DbContext.Participatings.FirstOrDefaultAsync(x => x.Id == id);
+        if (participation is null)
+            return NotFound();
+
+        return Ok(participation);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> InsertOrUpdate([FromBody] TeamModel? participation)
+    {
+        if (participation is null || participation.TeamId is null || participation.TournamentId is null )
+            return BadRequest();
+
+        return Ok(await DbContext.Participatings.AddAsync(participation));
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var participation = await DbContext.Participatings.FirstOrDefaultAsync(x => x.Id == id);
+        if (participation is null)
+            return NoContent();
+
+        if (participation.Team.LeaderId != LoggedUser.Id && participation.Tournament.CreatorId != LoggedUser.Id && !LoggedUser.IsAdministrator)
+            return Forbid();
+
+        DbContext.Remove(participation);
+        await DbContext.SaveChangesAsync();
+
+        return Ok();
+    }
+}
