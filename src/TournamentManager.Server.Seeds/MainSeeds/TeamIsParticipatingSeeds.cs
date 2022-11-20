4534d1387b2fb930511a8808e979ed7d49917563
honzav23 <janvaclavik@valachnet.cz>
2022-11-20 21:22:31 +0100

    Approving / Removing users functional

--- src/TournamentManager.Server.Seeds/MainSeeds/TeamIsParticipatingSeeds.cs
+++ src/TournamentManager.Server.Seeds/MainSeeds/TeamIsParticipatingSeeds.cs
@@ -1,6 +1,25 @@
-﻿namespace TournamentManager.Server.Seeds.MainSeeds;
+﻿using TournamentManager.Server.DAL;
+using TournamentManager.Server.DAL.Entities;
+namespace TournamentManager.Server.Seeds.MainSeeds;
 
 public static class TeamIsParticipatingSeeds
 {
+    public static readonly TeamIsParticipatingEntity Team1InAwesomeTournament = new(false)
+    {
+        TournamentId = TournamentSeeds.Tournament1.Id,
+        TeamId = TeamSeeds.JohnsTeam.Id
+    };
+
+    public static readonly TeamIsParticipatingEntity Team2InAwesomeTournament = new(false)
+    {
+        TournamentId = TournamentSeeds.Tournament1.Id,
+        TeamId = TeamSeeds.Team1.Id
+    };
     
+    public static void Seed(TournamentManagerDbContext dbContext)
+    {
+        dbContext.Participatings.Add(Team1InAwesomeTournament);
+        dbContext.Participatings.Add(Team2InAwesomeTournament);
+    }
+
 }
