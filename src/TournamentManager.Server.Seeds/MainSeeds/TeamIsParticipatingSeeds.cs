444761f2b3d44a829c0dad93844715821641f789
xdenka00 <xdenka00@vutbr.cz>
2022-11-26 15:00:31 +0100

    Approved team praticipation

--- src/TournamentManager.Server.Seeds/MainSeeds/TeamIsParticipatingSeeds.cs
+++ src/TournamentManager.Server.Seeds/MainSeeds/TeamIsParticipatingSeeds.cs
@@ -41,7 +41,7 @@ public static class TeamIsParticipatingSeeds
         TournamentId = TournamentSeeds.Tournament1.Id,
         TeamId = TeamSeeds.Team5.Id
     };
-    public static readonly TeamIsParticipatingEntity JohsInNYTournament = new(false,
+    public static readonly TeamIsParticipatingEntity JohsInNYTournament = new(true,
         DateTime.Parse("25 October 2022 15:40"))
     {
         TournamentId = TournamentSeeds.NY2024.Id,
