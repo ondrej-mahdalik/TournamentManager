63d10d83e61ca74a87b03ebc7ead098b129a9d60
honzav23 <janvaclavik@valachnet.cz>
2022-12-14 16:09:27 +0100

    Headers added

--- src/TournamentManager.Server.App/Controllers/TournamentController.cs
+++ src/TournamentManager.Server.App/Controllers/TournamentController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
